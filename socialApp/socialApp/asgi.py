003625dbf932fca8ddb1aa13a64b23df01d5c508
Harsh-404 <rishibarnwal1234@gmail.com>
2026-02-17 16:45:00 +0530

    fix(deploy): initialize django settings before importing apps in asgi.py

--- socialApp/socialApp/asgi.py
+++ socialApp/socialApp/asgi.py
@@ -7,23 +7,25 @@ For more information on this file, see
 https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
 """
 
+
 import os
-from channels.auth import AuthMiddlewareStack
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
 from django.core.asgi import get_asgi_application
-
-from socialApp import urls
-from account import routing
-
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'socialApp.settings')
 django_asgi_app = get_asgi_application()
 
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+
+from socialApp import urls
+from account import routing
+
 application = ProtocolTypeRouter({
-    'http': get_asgi_application(),
-    'websocket':
+    "http": django_asgi_app,
+    "websocket": AuthMiddlewareStack(
         URLRouter(
             routing.websocket_urlpatterns
         )
+    ),
 })
